26cc7aa4509edc17bcfe46cde1ff0a1d55e6478d
Karuppiah Natarajan <karuppiah7890@gmail.com>
2019-07-19 14:41:20 +0530

    use distributed db to sync position

--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module github.com/karuppiah7890/beep-demo
 go 1.12
 
 require (
+	github.com/buraksezer/consistent v0.0.0-20190524112033-8d954167dcc6 // indirect
+	github.com/buraksezer/olric v0.1.0
 	github.com/faiface/beep v1.0.1
 	github.com/golang/protobuf v1.3.2
+	github.com/hashicorp/logutils v1.0.0 // indirect
+	github.com/hashicorp/memberlist v0.1.4 // indirect
+	github.com/julienschmidt/httprouter v1.2.0 // indirect
 	google.golang.org/grpc v1.22.0
 )
--- main.go
+++ main.go
@@ -4,33 +4,15 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net"
 	"os"
 	"time"
 
 	"github.com/faiface/beep"
-	"google.golang.org/grpc"
 
+	olric "github.com/buraksezer/olric"
 	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/speaker"
-	"github.com/karuppiah7890/beep-demo/stream"
 )
-
-type streamServer struct {
-	streamer beep.StreamSeekCloser
-}
-
-func (s streamServer) Position(context.Context, *stream.PositionRequest) (*stream.PositionResponse, error) {
-	speaker.Lock()
-	position := s.streamer.Position()
-	speaker.Unlock()
-
-	return &stream.PositionResponse{Position: int64(position)}, nil
-}
-
-func newStreamServer(streamer beep.StreamSeekCloser) *streamServer {
-	return &streamServer{streamer: streamer}
-}
 
 func main() {
 	f, err := os.Open("derek-clegg-youre-the-dummy.mp3")
@@ -44,6 +26,7 @@ func main() {
 
 	defer streamer.Close()
 	done := make(chan bool)
+	shutdownDB := make(chan bool)
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 
 	argsLength := len(os.Args)
@@ -54,53 +37,133 @@ func main() {
 	}
 
 	if argsLength == 2 {
-		// Set up a connection to the server.
-		conn, err := grpc.Dial(os.Args[1], grpc.WithInsecure())
-		if err != nil {
-			log.Fatalf("did not connect: %v", err)
-		}
-		defer conn.Close()
+		// get position from olric
 
-		streamClient := stream.NewStreamClient(conn)
-
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-		defer cancel()
-
-		positionResponse, err := streamClient.Position(ctx, &stream.PositionRequest{})
-		if err != nil {
-			log.Fatalf("could not get stream position: %v", err)
+		m, _ := olric.NewMemberlistConfig("local")
+		m.BindAddr = "127.0.0.1"
+		m.BindPort = 5656
+		c := &olric.Config{
+			Name:             "127.0.0.1:3636",
+			Peers:            []string{"127.0.0.1:5555"},
+			MemberlistConfig: m,
 		}
 
-		if err = streamer.Seek(int(positionResponse.GetPosition())); err != nil {
+		db, err := olric.New(c)
+		if err != nil {
+			log.Fatalf("could not create db: %v\n", err)
+		}
+
+		go func() {
+			err := db.Start()
+			if err != nil {
+				panic(fmt.Sprintf("could not start db: %v\n", err))
+			}
+		}()
+
+		go func() {
+			<-shutdownDB
+			err := db.Shutdown(context.Background())
+			if err != nil {
+				panic(fmt.Sprintf("could not shutdown gracefully %v\n", err))
+			}
+			done <- true
+		}()
+
+		dm := db.NewDMap("beep-demo")
+		position, err := dm.Get("position")
+		if err != nil {
+			log.Fatalf("could not get position from db: %v", err)
+		}
+
+		seekPosition, ok := position.(int)
+		if !ok {
+			panic(fmt.Sprintf("got position value %v in a wrong data type", position))
+		}
+
+		if err = streamer.Seek(seekPosition); err != nil {
 			log.Fatalf("could not seek to position in stream: %v", err)
 		}
 
 		speaker.Play(beep.Seq(streamer, beep.Callback(func() {
-			done <- true
+			shutdownDB <- true
 		})))
 
-		<-done
-	} else if argsLength == 1 {
-		speaker.Play(beep.Seq(streamer, beep.Callback(func() {
-			done <- true
-		})))
-
-		lis, err := net.Listen("tcp", "localhost:8080")
-		if err != nil {
-			log.Fatalf("failed to listen: %v", err)
-		}
-		grpcServer := grpc.NewServer()
-		stream.RegisterStreamServer(grpcServer, newStreamServer(streamer))
-
+		ticker := time.NewTicker(100 * time.Millisecond)
 		go func() {
-			<-done
-			grpcServer.GracefulStop()
+			for range ticker.C {
+				position, err := dm.Get("position")
+				if err != nil {
+					log.Fatalf("could not get position from db: %v", err)
+				}
+
+				seekPosition, ok := position.(int)
+				if !ok {
+					panic(fmt.Sprintf("got position value %v in a wrong data type", position))
+				}
+
+				speaker.Lock()
+				if err = streamer.Seek(seekPosition); err != nil {
+					log.Fatalf("could not seek to position in stream: %v", err)
+				}
+				speaker.Unlock()
+			}
 		}()
 
-		err = grpcServer.Serve(lis)
-
-		if err != nil {
-			fmt.Printf("error occurred while starting grpc server : %v", err)
+		<-done
+		ticker.Stop()
+	} else if argsLength == 1 {
+		m, _ := olric.NewMemberlistConfig("local")
+		m.BindAddr = "127.0.0.1"
+		m.BindPort = 5555
+		c := &olric.Config{
+			Name:             "127.0.0.1:3535", // Unique in the cluster and used by TCP server.
+			Peers:            []string{"127.0.0.1:5656"},
+			MemberlistConfig: m,
 		}
+		db, err := olric.New(c)
+		if err != nil {
+			log.Fatalf("could not create db: %v\n", err)
+		}
+
+		go func() {
+			err := db.Start()
+			if err != nil {
+				panic(fmt.Sprintf("could not start db: %v\n", err))
+			}
+		}()
+
+		dm := db.NewDMap("beep-demo")
+		err = dm.Put("position", 0)
+		if err != nil {
+			log.Fatalf("could not store position into db: %v", err)
+		}
+
+		go func() {
+			<-shutdownDB
+			err := db.Shutdown(context.Background())
+			if err != nil {
+				panic(fmt.Sprintf("could not shutdown gracefully %v\n", err))
+			}
+			done <- true
+		}()
+
+		speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+			shutdownDB <- true
+		})))
+
+		ticker := time.NewTicker(100 * time.Millisecond)
+		go func() {
+			for range ticker.C {
+				speaker.Lock()
+				position := streamer.Position()
+				speaker.Unlock()
+
+				if err = dm.Put("position", position); err != nil {
+					log.Fatalf("could not store position into db: %v", err)
+				}
+			}
+		}()
+
+		<-done
 	}
 }
